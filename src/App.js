adfcb9b95663824a4e033750c6e02c8b2897e02e
Sergio Di Martino <sergio@webdevserg.io>
2019-05-15 09:00:31 -0700

    Converts index to React

--- src/App.js
+++ src/App.js
@@ -1,43 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-// Particles.js - This may not be utilized, but 
+// Particles.js - This may not be utilized, but
 // I am working on it to see if I can get it running //
 
 // import Particles from 'react-particles-js';
- 
+
 // class App extends Component{
-  
+
 //     render(){
 //         return (
 //             <Particles />
 //         );
 //     };
- 
+
 // }
-
-
 
 export default App;
