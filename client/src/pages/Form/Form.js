cddd3b00e8324ac76ecc7ed68af55bbf7b5875b1
Sergio Di Martino <sergio@webdevserg.io>
2019-05-23 00:50:29 -0700

    Begins converting input form into React

--- client/src/pages/Form/Form.js
+++ client/src/pages/Form/Form.js
@@ -9,8 +9,52 @@ class Form extends Component {
 
   render() {
     return (
-      <div className="jumbotron">
-        <h1 className="display-4">Hi</h1>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+          <div className="container">
+            <h1 className="header">VetSx &nbsp;</h1>
+            <div className="tagline">Care. Heal. Connect.</div>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarResponsive"
+              aria-controls="navbarResponsive"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" id="patientStat" href="#">
+                    Patient Status
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    | About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    | Consumer |
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="container mt-3">
+          <h2>Patient Status</h2>
+          <br />
+        </div>
       </div>
     );
   }
