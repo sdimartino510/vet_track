66b806930d600b383e5e72bf372c57c4dcb9999a
Sergio Di Martino <sergio@webdevserg.io>
2019-05-23 08:49:53 -0700

    Converting html to React for Form

--- client/src/pages/Form/Form.js
+++ client/src/pages/Form/Form.js
@@ -3,6 +3,7 @@ import { Redirect } from "react-router-dom";
 import "./form.css";
 
 class Form extends Component {
+  state = {};
   componentDidMount() {
     document.title = this.props.title;
   }
@@ -54,6 +55,138 @@ class Form extends Component {
         <div className="container mt-3">
           <h2>Patient Status</h2>
           <br />
+          <ul className="nav nav-tabs" id="myTab" role="tablist">
+            <li className="nav-item">
+              <a
+                className="nav-link active"
+                id="date-tab"
+                data-toggle="tab"
+                role="tab"
+                href="#date"
+                aria-controls="date"
+                aria-selected="true"
+              >
+                Date & Time
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                id="basic-tab"
+                data-toggle="tab"
+                role="tab"
+                href="#basic"
+                aria-controls="basic"
+                aria-selected="false"
+              >
+                Profile
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                id="doc-tab"
+                data-toggle="tab"
+                role="tab"
+                href="#doc"
+                aria-controls="doc"
+                aria-selected="false"
+              >
+                Providers
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                id="vitals-tab"
+                data-toggle="tab"
+                role="tab"
+                href="#vitals"
+                aria-controls="vitals"
+                aria-selected="false"
+              >
+                Vitals
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                id="surg-tab"
+                data-toggle="tab"
+                role="tab"
+                href="#surg"
+                aria-controls="surgery"
+                aria-selected="false"
+              >
+                Surgical Status
+              </a>
+            </li>
+          </ul>
+          {/* <!-- Tab Panes --> */}
+          {/* <!-- DATE & TIME --> */}
+          <div
+            className="tab-pane fade show active"
+            id="date"
+            role="tabpanel"
+            aria-labelledby="date-tab"
+          >
+            Date & Time
+          </div>
+          {/* <!-- PROFILE --> */}
+          <div
+            className="tab-pane fade"
+            id="basic"
+            role="tabpanel"
+            aria-labelledby="basic-tab"
+          >
+            {/* <!-- Patient Information Header --> */}
+            <div className="container-profile">
+              <div className="page-header">
+                <div>Profile Information</div>
+              </div>
+            </div>
+            {/* <!-- Patient Image --> */}
+            <div className="media">
+              <img
+                src="../client/public/assets/images/shiba.jpeg"
+                className="mr-4"
+                alt="shibainu"
+              />
+              <div className="media-body">
+                <br />
+                <br />
+                {/* <!-- Patient and Client ID Input --> */}
+                <form>
+                  <div className="form-group">
+                    <label id="patientID" for="patientID">
+                      Patient ID
+                    </label>
+                    <input
+                      className="form-control input-sm col-md-4"
+                      id="patientText"
+                      type="text"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label id="clientID" for="clientID">
+                      Client ID
+                    </label>
+                    <input
+                      className="form-control input-sm col-md-4"
+                      id="clientText"
+                      type="text"
+                    />
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+          {/* <!-- Patient Information Header --> */}
+          <div className="container-profile">
+            <div className="patient-header">
+              <div>Patient Information</div>
+            </div>
+          </div>
         </div>
       </div>
     );
