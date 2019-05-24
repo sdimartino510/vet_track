d93c72c06e669a9071dc76cf146900fccddc98ac
Silas 01 <Mnavid02@gmail.com>
2019-05-23 20:18:49 -0700

    created schemas for login and pet status

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,6 @@
-const router = require("express").Router();
+const routes = require("express").Router();
 const routes = require("./routes");
+const User = require('../model/user');
 
 // routes
 router.use(routes);
--- routes/server.js
+++ routes/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 27017;
+const PORT = process.env.PORT || 3001;
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
