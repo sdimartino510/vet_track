82cee961484e3b6ccd3d770b59400a8d668b07ca
Sergio Di Martino <sergio@webdevserg.io>
2019-05-29 02:26:30 -0700

    Completes form page

--- client/src/pages/Consumer/Consumer.js
+++ client/src/pages/Consumer/Consumer.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "./consumer.css";
 
 class Consumer extends Component {
   state = {
--- routes/api/routes.js
+++ routes/api/routes.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+const user = require("../../model/user");
 
 router.route("/login").post((req, res) => {
   console.log("req.body", req.body);
+  res.status(200).json({ result: "success" });
+});
+router.route("/profile").post((req, res) => {
+  console.log("req.body - profile", req.body);
   res.status(200).json({ result: "success" });
 });
 
